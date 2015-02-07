1c1ae1c0d5d5cf92cb51e9cd5ee5537bb91cb536
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-02-07 09:57:02 +0000

    Chapter 1

---
+++ NOTES.rb
@@ -0,0 +1,32 @@
+# Object oriented design requires that you shift from thinking of the world as a collection of predefined processes to modelling the world as a series of messages that pass between objects.
+
+# Object oriented applications are made up of parts that interact to produce the behaviour of the whole. The parts are OBJECTS; interactions are embodied in MESSAGES that pass between them.
+
+# Object oriented applications are made up of objects and the messages that pass between them.
+# => People often focus too much on the objects, and not enough on the messages passing between them. In well designed applications, the latter ranks higher.
+
+# Objects are instantiated instances of abstracts classes. They have behaviours (methods) and may contain data (variables, data to which they alone control access. Objects invoke one anothers behavious by sending each other MESSAGES
+
+# OOD is about managing DEPENDENCIES
+
+# TOOLBOX:
+
+# SOLID
+
+# Single responsibility
+# Open-Closed
+# Liskov substitution
+# Interface segregation
+# Dependency inversion
+
+# DRY - Don't Repeat Yourself
+# LoD - Law of Demeter
+
+# DESIGN PATTERNS
+
+# See later :)
+
+
+#  AGILE & Iterative Development
+#  Test Driven Development
+#
